406483e5649568c04c30dadda1ed1894b3419e4e
jackyli <jackyli@qiyi.com>
2017-07-31 11:05:01 +0800

    解决ViewPager循环的问题，实现自动循环

--- AQYSimulate/app/src/main/java/com/zhuang/jackyli/aqysimulate/model/ViewModel9.java
+++ AQYSimulate/app/src/main/java/com/zhuang/jackyli/aqysimulate/model/ViewModel9.java
@@ -1,10 +1,8 @@
 package com.zhuang.jackyli.aqysimulate.model;
 
+import android.content.Context;
 import android.net.Uri;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +12,10 @@ import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.zhuang.jackyli.aqysimulate.R;
-import com.zhuang.jackyli.aqysimulate.adapter.MyPagerAdapter;
-import com.zhuang.jackyli.aqysimulate.ui.MyCycleViewPager;
+import com.zhuang.jackyli.aqysimulate.adapter.CyclePagerAdapter;
+import com.zhuang.jackyli.aqysimulate.ui.CycleViewPager;
 import com.zhuang.jackyli.aqysimulate.ui.ScaleTransformer;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,27 +23,70 @@ import java.util.List;
  */
 
 public class ViewModel9 implements AbsModel {
-    public final static String TAG = "ViewModel999999999";
+    public Context mContext;
+    private static ViewModel9Holder mModel9Holder;
+    public SimpleDraweeView mLeftPicImageview, mLeftRightUpPic;
+    public TextView mLeftRightBottomText, mLeftLeftBottomText, mLeftUpTitle, mLeftDownTitle;
 
-    private ViewModel9Holder mModel9Holder;
-    SimpleDraweeView mLeftPicImageview, mLeftRightUpPic;
-    TextView mLeftRightBottomText, mLeftLeftBottomText, mLeftUpTitle, mLeftDownTitle;
     @Override
     public void onBindData(RecyclerView.ViewHolder holder, ViewModel viewModel) {
         mModel9Holder = (ViewModel9Holder) holder;
-        View view;
+        mContext = mModel9Holder.myCycleViewPager.parent.getContext();
         List<BaseModel> lists = viewModel.getBaseModels();
-        final List<View> views = new ArrayList<>();
-        
-        for (BaseModel baseModel : lists) {
-            view = LayoutInflater.from(mModel9Holder.myCycleViewPager.parent.getContext()).inflate(R.layout.base_type9_layout, mModel9Holder.myCycleViewPager, false);
-            mLeftPicImageview = (SimpleDraweeView) view.findViewById(R.id.big_pic);
-            mLeftRightUpPic = (SimpleDraweeView) view.findViewById(R.id.right_up_pic);
-            mLeftRightBottomText = (TextView) view.findViewById(R.id.right_bottom_text);
-            mLeftLeftBottomText = (TextView) view.findViewById(R.id.left_bottom_text);
-            mLeftUpTitle = (TextView) view.findViewById(R.id.up_title);
-            mLeftDownTitle = (TextView) view.findViewById(R.id.down_title);
-            
+        SimpleBannerAdapter pagerAdapter = new SimpleBannerAdapter(lists, mContext);
+        mModel9Holder.myCycleViewPager.setPageMargin(5);
+        mModel9Holder.myCycleViewPager.setOffscreenPageLimit(3);//显示左右两个viewpager
+        mModel9Holder.myCycleViewPager.setPageTransformer(false, new ScaleTransformer());//缩放效果
+        mModel9Holder.myCycleViewPager.setAdapter(pagerAdapter);
+        mModel9Holder.myCycleViewPager.startAutoScroll();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder getViewHolder(ViewGroup parent, int modelType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.type9_recyclerview_item, parent, false);
+        mModel9Holder = new ViewModel9Holder(view, modelType, parent);
+        return mModel9Holder;
+    }
+
+
+    public static class ViewModel9Holder extends RecyclerView.ViewHolder {
+        CycleViewPager myCycleViewPager;
+
+        public ViewModel9Holder(View itemView, int modelType, ViewGroup parent) {
+            super(itemView);
+            myCycleViewPager = (CycleViewPager) itemView.findViewById(R.id.my_lunbo_viewpager);
+            myCycleViewPager.setParent(parent);
+        }
+    }
+
+    class SimpleBannerAdapter extends CyclePagerAdapter {
+
+        private List<BaseModel> lists;
+        private Context mContext;
+
+        public SimpleBannerAdapter(List<BaseModel> lists, Context mContext) {
+            this.lists = lists;
+            this.mContext = mContext;
+        }
+
+        @Override
+        public int getRealCount() {
+            return lists.size();
+        }
+
+        @Override
+        public View getViewAtRealPosition(final int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.base_type9_layout, container, false);
+            }
+            BaseModel baseModel = lists.get(position);
+            mLeftPicImageview = (SimpleDraweeView) convertView.findViewById(R.id.big_pic);
+            mLeftRightUpPic = (SimpleDraweeView) convertView.findViewById(R.id.right_up_pic);
+            mLeftRightBottomText = (TextView) convertView.findViewById(R.id.right_bottom_text);
+            mLeftLeftBottomText = (TextView) convertView.findViewById(R.id.left_bottom_text);
+            mLeftUpTitle = (TextView) convertView.findViewById(R.id.up_title);
+            mLeftDownTitle = (TextView) convertView.findViewById(R.id.down_title);
+
             mLeftDownTitle.setText(baseModel.getSecondText());
             mLeftUpTitle.setText(baseModel.getFirstText());
             mLeftLeftBottomText.setText(baseModel.getThirdText());
@@ -59,37 +99,7 @@ public class ViewModel9 implements AbsModel {
                     .build();
             mLeftPicImageview.setAspectRatio(1.8f);
             mLeftPicImageview.setController(draweeController);
-            views.add(view);
-
-        }
-        Log.d(TAG, "onBindData: views.size"+views.size());
-
-        PagerAdapter pagerAdapter = new MyPagerAdapter(views);
-        mModel9Holder.myCycleViewPager.setPageMargin(5);
-        mModel9Holder.myCycleViewPager.setOffscreenPageLimit(3);//显示左右两个viewpager
-        mModel9Holder.myCycleViewPager.setPageTransformer(false,new ScaleTransformer());//缩放效果
-        mModel9Holder.myCycleViewPager.setAdapter(pagerAdapter);
-        //int firstPage = Integer.MAX_VALUE / 2 / views.size() * views.size();
-        mModel9Holder.myCycleViewPager.setCurrentItem(399, false);
-    }
-
-    @Override
-    public   RecyclerView.ViewHolder getViewHolder(ViewGroup parent,int modelType) {
-       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.type9_recyclerview_item, parent, false);
-        mModel9Holder = new ViewModel9Holder(view, modelType, parent);
-        return mModel9Holder;
-    }
-
-
-
-
-
-    public static class ViewModel9Holder extends RecyclerView.ViewHolder {
-        MyCycleViewPager myCycleViewPager;
-        public ViewModel9Holder(View itemView,int modelType,ViewGroup parent) {
-            super(itemView);
-            myCycleViewPager = (MyCycleViewPager) itemView.findViewById(R.id.my_lunbo_viewpager);
-            myCycleViewPager.setParent(parent);
+            return convertView;
         }
     }
 }
--- AQYSimulate/app/src/main/java/com/zhuang/jackyli/aqysimulate/ui/ScaleTransformer.java
+++ AQYSimulate/app/src/main/java/com/zhuang/jackyli/aqysimulate/ui/ScaleTransformer.java
@@ -9,11 +9,11 @@ import android.view.View;
 
 public class ScaleTransformer implements ViewPager.PageTransformer{
     private static final float MIN_SCALE = 0.95f;
-    private static final float MIN_ALPHA = 0.5f;
+   // private static final float MIN_ALPHA = 0.5f;
     @Override
     public void transformPage(View page, float position) {
         if (position < -1 || position > 1) {
-            page.setAlpha(MIN_ALPHA);
+           // page.setAlpha(MIN_ALPHA);
             page.setScaleX(MIN_SCALE);
             page.setScaleY(MIN_SCALE);
         } else if (position <= 1) { // [-1,1]
@@ -28,7 +28,7 @@ public class ScaleTransformer implements ViewPager.PageTransformer{
                 page.setScaleX(scaleX);
                 page.setScaleY(scaleX);
             }
-            page.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+          //  page.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
         }
     }
 }
